3ac9fd091037ca2681d5bcdf11ed812fcd8edad6
Pedro Rodrigues <pe-rique@hotmail.com>
2017-05-23 21:50:38 -0300

    Pedro
    Descrição e imgs

--- arquivos/all.php
+++ arquivos/all.php
@@ -1,4 +1,4 @@
-<a href="livros/AJR/ajr.html"><img src="arquivos/img/rajy.jpg" style="width: 10%; height: 180px; margin-left: 26%; margin-top: 10%;"></a>
-<a href="livros/AJF/ajf.html"><img src="arquivos/img/janela.gif" style="width: 10%; height: 180px; margin-left: 10%; margin-top: 10%;"></a>
-<a href="livros/WT/wt.html"><img src="arquivos/img/wt.jpg" style="width: 10%; height: 180px; margin-left: 10%; margin-top: 10%;"></a>
+<a href="livros/AJR/ajr.html"><img src="arquivos/img/Joias de Rajy.png" style="width: 10%; height: 180px; margin-left: 26%; margin-top: 10%;"></a>
+<a href="livros/AJF/ajf.html"><img src="arquivos/img/A Janela da Frente.png" style="width: 10%; height: 180px; margin-left: 10%; margin-top: 10%;"></a>
+<a href="livros/WT/wt.html"><img src="arquivos/img/Gotas da Guerra.png" style="width: 10%; height: 180px; margin-left: 10%; margin-top: 10%;"></a>
 <img src="arquivos/estante.png" style="margin-left: 20% !important; width: 65% !important; margin-top: -9.5%;">
